3864a215c5ec9aaea3f00a91f33abbd1671f0119
Talles andré <tallesandre15@gmail.com>
2025-02-13 19:28:46 -0300

    prototipo da tela de vendas

--- src/pages/venda/Venda.jsx
+++ src/pages/venda/Venda.jsx
@@ -1,20 +1,17 @@
 import Header from '../../componentes/header/Header';
+import './Venda.css';
+import HeaderBar from '../../componentes/HeaderBar/HeaderBar';
+import SearchBar from '../../componentes/SearchBar/SearchBar';
 
 export default function Inicio() {
   return (
     <>
-      <section id="admin-section">
+      <section id="admin-section" >
         <Header />
-        
-        {/*
-        
-        
-            Aqui ficará p codigo da tela de venda
-        
-        
-        
-        */}
-
+        <div style={{width:'100%'}}>
+          <HeaderBar title='Vendas' />
+          <SearchBar />
+        </div>
       </section>
     </>
   );
