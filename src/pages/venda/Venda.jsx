4a49b6717cb8d537c42a5c672ab083f88dba5a5e
Talles andré <tallesandre15@gmail.com>
2025-02-16 14:06:43 -0300

    usar classes bootstrap

--- src/pages/venda/Venda.jsx
+++ src/pages/venda/Venda.jsx
@@ -1,14 +1,13 @@
 import Header from '../../componentes/header/Header';
-import './Venda.css';
 import HeaderBar from '../../componentes/HeaderBar/HeaderBar';
 import SearchBar from '../../componentes/SearchBar/SearchBar';
 
 export default function Inicio() {
   return (
     <>
-      <section id="admin-section" >
+      <section id="admin-section" className="d-flex flex-row">
         <Header />
-        <div style={{width:'100%'}}>
+        <div className="w-100 p-3">
           <HeaderBar title='Vendas' />
           <SearchBar />
         </div>
