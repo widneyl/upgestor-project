84ef9a145a825080c39a94a0b267a28b867ff2fa
josiasdev <francisco.batista67@alu.ufc.br>
2025-02-17 13:57:11 -0300

    feat: separando o css do arquivo jsx

--- src/componentes/HeaderBar/HeaderBar.jsx
+++ src/componentes/HeaderBar/HeaderBar.jsx
@@ -5,9 +5,9 @@ import seta from '../../img/seta_para_baixo.svg';
 // barra de cabeçalho com titulo e botao para o usuario
 export default function HeaderBar({ title }) {
     return <div className='row-flex HeaderBar'>
-        <h1 style={{fontSize: '30px'}}>{title}</h1>
+        <h1>{title}</h1>
         <div className='row-flex'>
-            <img src={userimg} alt="" />
+            <img src={userimg} alt="imagem de usuário" />
             <p>Usuario</p>
             <img src={seta} className="seta"/>
         </div>
