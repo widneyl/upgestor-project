d90ba912d03c77787e9038033248a4e7bf7c1ef7
Talles andré <tallesandre15@gmail.com>
2025-02-17 11:59:24 -0300

    finalizar componentes da ui

--- src/componentes/HeaderBar/HeaderBar.jsx
+++ src/componentes/HeaderBar/HeaderBar.jsx
@@ -1,15 +1,23 @@
-import './HeaderBar.css';
-import userimg from '../../img/user.png';
-import seta from '../../img/seta_para_baixo.svg';
+import "./HeaderBar.css";
+import userimg from "../../img/user.png";
+import DropDownMenu from "../DropDownMenu/DropDownMenu";
 
 // barra de cabeçalho com titulo e botao para o usuario
 export default function HeaderBar({ title }) {
-    return <div className='row-flex HeaderBar'>
-        <h1 style={{fontSize: '30px'}}>{title}</h1>
-        <div className='row-flex'>
-            <img src={userimg} alt="" />
-            <p>Usuario</p>
-            <img src={seta} className="seta"/>
-        </div>
+  return (
+    <div className="HeaderBar">
+      <h1 style={{ fontSize: "30px" }}>{title}</h1>
+      <div className="d-flex gap-3">
+        <img src={userimg} alt="" />
+        <DropDownMenu
+          nome="Usuario"
+          options={[
+            { key: "Minha conta" },
+            { key: "Configuraçoes" },
+            { key: "Sair" },
+          ]}
+        />
+      </div>
     </div>
+  );
 }
--- src/pages/venda/Venda.jsx
+++ src/pages/venda/Venda.jsx
@@ -1,17 +1,18 @@
-import Header from '../../componentes/header/Header';
-import HeaderBar from '../../componentes/HeaderBar/HeaderBar';
-import SearchBar from '../../componentes/SearchBar/SearchBar';
+import Header from "../../componentes/header/Header";
+import HeaderBar from "../../componentes/HeaderBar/HeaderBar";
+import SearchBar from "../../componentes/SearchBar/SearchBar";
+import Table from "../../componentes/tabelaDeProdutos/TabelaDeProdutos";
+import "./style.css";
 
 export default function Inicio() {
   return (
-    <>
-      <section id="admin-section" className="d-flex flex-row">
-        <Header />
-        <div className="w-100 p-3">
-          <HeaderBar title='Vendas' />
-          <SearchBar />
-        </div>
-      </section>
-    </>
+    <section id="admin-section" className="d-flex">
+      <Header />
+      <div className="w-100 p-3">
+        <HeaderBar title="Vendas" />
+        <SearchBar />
+        <Table/>
+      </div>
+    </section>
   );
 }
