05b6f4474dc0ea465a6c9b6a766436c881d43920
josiasdev <francisco.batista67@alu.ufc.br>
2025-02-17 14:46:08 -0300

    feat: Chamando os componentes em vendas

--- src/pages/venda/Venda.jsx
+++ src/pages/venda/Venda.jsx
@@ -2,6 +2,8 @@ import Header from '../../componentes/header/Header';
 import './Venda.css';
 import HeaderBar from '../../componentes/HeaderBar/HeaderBar';
 import SearchBar from '../../componentes/SearchBar/SearchBar';
+import TabelaDeProdutos from '../../componentes/tabelaDeProdutos/TabelaDeProdutos';
+import ServicoPrestado from '../../componentes/servicoPrestado/servicoPrestado';
 
 export default function Inicio() {
   return (
@@ -11,6 +13,8 @@ export default function Inicio() {
         <div style={{width:'100%'}}>
           <HeaderBar title='Vendas' />
           <SearchBar />
+          <TabelaDeProdutos />
+          <ServicoPrestado/>
         </div>
       </section>
     </>
