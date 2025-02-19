574d32bdfb105595dd620094a8139a6e8f35b531
Talles andré <tallesandre15@gmail.com>
2025-02-19 14:52:27 -0300

    adicionar layout grid aos graficos da tela de gestao

--- src/pages/gestao/Gestao.jsx
+++ src/pages/gestao/Gestao.jsx
@@ -1,37 +1,29 @@
-import CardInicio from '../../componentes/cardInicio/CardInicio';
-import Header from '../../componentes/header/Header';
-import HeaderBar from '../../componentes/HeaderBar/HeaderBar'
-import Chart from '../../componentes/graficos/FinancasGraficos/Chart'
+import CardInicio from "../../componentes/cardInicio/CardInicio";
+import Header from "../../componentes/header/Header";
+import HeaderBar from "../../componentes/HeaderBar/HeaderBar";
+import Chart from "../../componentes/graficos/FinancasGraficos/Chart";
 
-import img1 from '../../img/carrinho-verde.png';
-import img2 from '../../img/carrinho-azul.png';
-import img3 from '../../img/carrinho-amarelo.png';
+import img1 from "../../img/carrinho-verde.png";
+import img2 from "../../img/carrinho-azul.png";
+import img3 from "../../img/carrinho-amarelo.png";
 
-import './style.css';
-import { useState } from 'react';
-import QuantidadePorCategoria from '../../componentes/graficos/FinancasGraficos/QuantidadePorCategoria';
+import "./style.css";
+import { useState } from "react";
+import QuantidadePorCategoria from "../../componentes/graficos/FinancasGraficos/QuantidadePorCategoria";
 
 export default function Inicio() {
-
-  const [exibicaoPadrao, setExibicaoPadrao] = useState(true)
-
-
-
+  const [exibicaoPadrao, setExibicaoPadrao] = useState(true);
 
   return (
     <>
-      <section id="admin-section" className='d-flex'>
-
+      <section id="admin-section" className="d-flex">
         <Header />
 
         <div id="section-content" className="p-2 d-flex w-100">
-
-
-          <div className='container-fluid area-cards'>
-
+          <div className="container-fluid area-cards">
             <HeaderBar title={"Gestão"} />
 
-            <div className='lista-card d-flex gap-5 p-3'>
+            <div className="lista-card d-flex gap-5 p-3">
               <CardInicio
                 titulo={"Finanças"}
                 descricao={"Veja como vão suas finanças"}
@@ -46,7 +38,7 @@ export default function Inicio() {
                 urlImg={img2}
                 corCard={"#005aa6"}
                 corRodape={"#00447d"}
-              irPara={"/estoque"} // exemplo de navegação
+                irPara={"/estoque"} // exemplo de navegação
               />
               <CardInicio
                 titulo={"Exemplo"}
@@ -54,27 +46,19 @@ export default function Inicio() {
                 urlImg={img3}
                 corCard={"#a2a205"}
                 corRodape={"#848400"}
-              // irPara={"/ajustes"} // exemplo de navegação
+                // irPara={"/ajustes"} // exemplo de navegação
               />
             </div>
 
-            {
-              exibicaoPadrao ? (
-                <div className='lista-graf d-flex p-3 gap-5 '>
-                  <Chart />
-                  <Chart />
-                  <QuantidadePorCategoria/>
-                </div>
-
-
-              )
-                :
-                (
-                  <p>Ola</p>
-                )
-            }
-
-
+            {exibicaoPadrao ? (
+              <div className="lista-graf p-3 gap-5">
+                <Chart />
+                <Chart />
+                <QuantidadePorCategoria />
+              </div>
+            ) : (
+              <p>Ola</p>
+            )}
           </div>
         </div>
       </section>
