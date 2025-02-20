e8d633fedf0d7276328d477ce83f1b0505de2089
Talles andré <tallesandre15@gmail.com>
2025-02-20 16:31:03 -0300

    adicionar prototipo da tela de pagamento

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,31 +5,21 @@ import Gestao from './pages/gestao/Gestao'
 import Estoque from "./pages/gestao/estoque/Estoque";
 import LoginScreen from "./pages/login/LoginScreen";
 import Financas from "./pages/gestao/financas/Financas";
+import Pagamento from "./pages/venda/pagamento/Pagamento";
 
 
 export default function Rotas() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/"
-                    element={<Inicio />}
-                />
-                <Route path="/venda"
-                    element={<Venda />}
-                />
-                <Route path="/gestao"
-                    element={<Gestao />}
-                />
-                <Route path="/estoque"
-                    element={<Estoque />}
-                />
-                <Route path="/login"
-                    element={<LoginScreen/>} 
-                />
-                <Route path="/financas"
-                    element={<Financas/>} 
-                />
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/venda" element={<Venda />} />
+          <Route path="/gestao" element={<Gestao />} />
+          <Route path="/estoque" element={<Estoque />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/financas" element={<Financas />} />
+          <Route path="/pay" element={<Pagamento/>} />
+        </Routes>
+      </BrowserRouter>
     );
 }
